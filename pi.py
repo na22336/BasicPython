ea8ac265d67df44af8ca4c03115e69289e1d84d7
na22336 <k.isa.24.8.1564@4nonome.com>
2024-09-27 17:24:33 +0900

    Python入門の解答

--- pi.py
+++ pi.py
@@ -4,4 +4,13 @@ text = """
 """
 
 # TODO
-
+list1 = list(map(len, ["How", "I", "want", "a", "drink", "alcoholic", "of", "course", "after", "the", "heavy", "chapters", "involving", "quantum", "mechanics", "All","of","thy","geometry", "Herr","Planck", "is" ,"fairly" ,"hard"
+]))
+print(str(list1[0]) + str(list1[1]) + str(list1[2]) 
+      + str(list1[3]) + str(list1[4]) + str(list1[5])
+      + str(list1[6]) + str(list1[7]) + str(list1[8])
+      + str(list1[9]) + str(list1[10]) + str(list1[11])
+      + str(list1[12]) + str(list1[13]) + str(list1[14])
+      + str(list1[15]) + str(list1[16]) + str(list1[17])
+      + str(list1[18]) + str(list1[19]) + str(list1[20])
+      + str(list1[21]) + str(list1[22]) + str(list1[23]))
