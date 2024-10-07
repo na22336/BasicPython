10d2ee718c16b543645f679d4bd0bb56cc5f5858
na22336 <k.isa.24.8.1564@4nonome.com>
2024-10-07 11:29:55 +0900

    制御構文の解答

--- prime_number.py
+++ prime_number.py
@@ -1,5 +1,5 @@
-a = input("aの値を入力: ")
-b = input("bの値を入力: ")
+a = input("aの値を入力:")
+b = input("bの値を入力:")
 
 # TODO
 import math
@@ -7,8 +7,11 @@ a = int(a)
 b = int(b)
 # (1)
 if a == 1:
-        print('aは素数ではありません')
-        exit()
+    print('aは素数ではありません')
+    exit()
+elif a == 2 or a == 3 or a == 5 or a == 7:
+    print('aは素数です')
+    exit()
 
 elif a % 2 != 0 and a % 3 != 0 and a % 5 != 0 and a % 7 != 0:
         print('aは素数です')
@@ -20,14 +23,20 @@ else:
 
 # (2)
 if b == 1:
-        print('bは素数ではありません')
-        exit()
-elif b % 2 != 0 and b % 3 != 0 and b % 5 != 0 :
+    print('bは素数ではありません')
+    exit()
+elif b == 2 or b == 3 or b == 5 or b == 7:
+    print('bは素数です')
+    exit()
+
+elif b % 2 != 0 and b % 3 != 0 and b % 5 != 0 and b % 7 != 0:
         print('bは素数です')
         exit()
+
 else:
         print('bは素数ではありません')
         exit()
+
 
 # 補遺
 #for i in range(1,math.ceil(math.sqrt(b))):
